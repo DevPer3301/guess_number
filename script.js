5d38791eac4216517e5c36a15ce10316a8ee0c9b
DevPer3301 <devper3301@gmail.com>
2021-12-15 11:16:30 +0200

    Guess Number Game v#2

--- script.js
+++ script.js
@@ -1,29 +1,115 @@
-let random_number  = Number(Math.trunc(Math.random() * 20) + 1);
 
-console.log(random_number);
+
+$("#show_dropdown_btn").click(function(){
+    let dropdown_div = $("#dropdown_div");
+    if (dropdown_div.css("display") !== "block"){
+        dropdown_div.css({"display": "block"});
+    }else{
+        dropdown_div.css({"display": "none"});
+    }
+
+    $(".choose_item").click(function(){
+        let dropdown_str = String($(this).text());
+
+        for (let i = 0; i < dropdown_str.length; i++){
+            if (dropdown_str.indexOf(" ") != -1){
+                dropdown_str = dropdown_str.replace(" ", "");
+            }
+        }
+        $("#dropdown_inp").val(dropdown_str);
+        dropdown_div.css({"display": "none"});
+    });
+});
+
+
+$("#choose_btn").click(function(){
+    console.log($("#dropdown_inp").val());
+});
+let generate_number = function(){
+    window.random_number  = Number(Math.trunc(Math.random() * 20) + 1);
+    console.log(window.random_number);
+};
+
+
+let rand = window.addEventListener("load", function(){
+    generate_number();
+});
+
+
+
+document.querySelector("#again_btn").addEventListener("click", function(){
+    let score = document.querySelector("#score_value").textContent = "20";
+    let body = document.querySelector("body").style.backgroundColor = "rgb(24,24,24)";
+    document.querySelector("#number_input").value = "";
+    document.querySelector("#secret_num_p").textContent  = "?";
+    document.querySelector("#guessing_message").textContent = "Start guessing ...";
+    console.log("Restarted")
+    generate_number();
+});
+
 let get_score_value = function(){
     return document.querySelector("#score_value").textContent;
 }
 
-document.querySelector("#button_submit").addEventListener("click", function(){
+
+
+
+function event_main_code(rand_num = ""){
+    console.log(rand_num);
+    if (rand_num !== ""){
+        let random_number = Number(rand_num);
+        console.log("Here")
+    }
     let input_value = document.querySelector("#number_input").value;
     let guessing_message = document.querySelector("#guessing_message");
     let score = document.querySelector("#score_value");
-    if (input_value !== ""){
-        if (random_number === Number(input_value)){
-            let body = document.querySelector("body").style.backgroundColor = "rgb(37, 162, 29)";
-            let secret_number_p = document.querySelector("#secret_num_p").textContent = input_value;
-            let high_score_value = document.querySelector("#highscore_value").textContent = get_score_value();
-            guessing_message.textContent = "Correct number !!!";
-        }else if (random_number > Number(input_value)){
-            guessing_message.textContent = "Too low";
-            score.textContent = get_score_value() - 1;
+    let score_value = Number(score.textContent);
 
-        }else if (random_number < Number(input_value)){
-            guessing_message.textContent = "Too high";
-            score.textContent = get_score_value() - 1;
+    if (score_value !== 0){
+        if (input_value !== ""){
+            if (random_number === Number(input_value)){
+                let body = document.querySelector("body").style.backgroundColor = "rgb(37, 162, 29)";
+                let secret_number_p = document.querySelector("#secret_num_p").textContent = input_value;
+                let high_score_value = document.querySelector("#highscore_value");
+
+                if (Number(high_score_value.textContent) < Number(get_score_value())){
+                    high_score_value.textContent = get_score_value();
+                }
+
+                guessing_message.textContent = "Correct number !!!";
+            }else if (random_number > Number(input_value)){
+                guessing_message.textContent = "Too low";
+                score.textContent = get_score_value() - 1;
+                console.log(score.textContent);
+
+            }else if (random_number < Number(input_value)){
+                guessing_message.textContent = "Too high";
+                score.textContent = get_score_value() - 1;
+                console.log(score.textContent);
+
+            }
+        }else if (input_value === ""){
+            alert("Enter number in the input field!");
         }
-    }else if (input_value === ""){
-        alert("Enter number in the input field!");
+    }else if (score_value === 0){
+        let body = document.querySelector("body").style.backgroundColor = "rgb(252, 0, 6)";
+        guessing_message.textContent = "You lost the game !!!";
+    }
+}
+document.querySelector("#button_submit").addEventListener("click", function(){
+    alert("Clicked");
+    event_main_code();
+});
+
+document.querySelector("#number_input").addEventListener("keyup", function(event){
+    if (event.keyCode === 13){
+        event_main_code();
     }
 });
+
+
+
+
+
+
+
